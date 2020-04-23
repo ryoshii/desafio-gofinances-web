f3e223a2c36a74cb255762d627a3f2b4cea72987
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-04-23 17:06:59 -0300

    Bug typed files with File[]

--- src/components/Upload/index.tsx
+++ src/components/Upload/index.tsx
@@ -27,7 +27,10 @@ const Upload: React.FC<UploadProps> = ({ onUpload }: UploadProps) => {
 
   return (
     <>
-      <Dropzone accept="text/csv" onDropAccepted={(files) => onUpload(files)}>
+      <Dropzone
+        accept="text/csv"
+        onDropAccepted={(files: File[]) => onUpload(files)}
+      >
         {({ getRootProps, getInputProps, isDragActive, isDragReject }): any => (
           <DropContainer
             {...getRootProps()}
